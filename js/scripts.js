59485a6d401891093b9902e5ddcc4422ade9202c
Dorota Kazak <dorota.kazak@gmail.com>
2018-04-29 22:45:46 +0200

    Add scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+
+var a = prompt('wpisz pierwsza dowolna liczbe');
+var b = prompt('wpisz druga dowolna liczbe');
+var value = (a * a) - (2 * a * b) - (b * b) ;
+alert('Gratulacje! Twój wynik to: ' + value);
+console.log('Twoj wynik ze zmnienna a: ' + a + ' i zmienna b: ' + b + ' rowna sie: ' + value);
+/*alert('Wiesz co, twoj wynik to: ' + document.write);*/
+console.log('Twoj wynik ze zmnienna a: ' + a + ' i zmienna b: ' + b + ' rowna sie: ' + document.write);
+
+if (value > 0) {
+    document.write('wynik dodatni');
+} else if (value < 0) {
+    document.write('wynik ujemny');
+} else {
+    document.write('Aha sprawdzasz liczbe ' + value);
+}
