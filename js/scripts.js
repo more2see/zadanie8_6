7f1a96a9e0df44ee69cc324ed625215d90295cc7
Dorota Kazak <dorota.kazak@gmail.com>
2018-05-07 17:57:59 +0200

    Fix js

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
-
 var a = prompt('wpisz pierwsza dowolna liczbe');
 var b = prompt('wpisz druga dowolna liczbe');
 var value = (a * a) - (2 * a * b) - (b * b);
 alert('Gratulacje! Twój wynik to: ' + value);
 console.log('Twoj wynik ze zmnienna a: ' + a + ' i zmienna b: ' + b + ' rowna sie: ' + value);
-console.log('Twoj wynik ze zmnienna a: ' + a + ' i zmienna b: ' + b + ' rowna sie: ' + document.write);
-if (value > 0) {
-    document.write('wynik dodatni');
-} else if (value < 0) {
-    document.write('wynik ujemny');
-} else {
-    document.write('Aha sprawdzasz liczbe ' + value);
+function estimateValue () {
+	if (value > 0) {
+    return 'wynik dodatni';
+	} else if (value < 0) {
+    return 'wynik ujemny';
+	} else {
+    return ('Aha sprawdzasz liczbe ' + value);
+	}
 }
+console.log('Twoj wynik ze zmnienna a: ' + a + ' i zmienna b: ' + b + ' rowna sie: ' + estimateValue());
