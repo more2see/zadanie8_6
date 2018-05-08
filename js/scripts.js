a97f2e9b4d450f20e7d74f8487a649fabf8388b5
more2see <38584266+more2see@users.noreply.github.com>
2018-05-08 20:09:11 +0200

    Fix Tabs

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,13 @@ var b = prompt('wpisz druga dowolna liczbe');
 var value = (a * a) - (2 * a * b) - (b * b);
 alert('Gratulacje! Twój wynik to: ' + value);
 console.log('Twoj wynik ze zmnienna a: ' + a + ' i zmienna b: ' + b + ' rowna sie: ' + value);
-function estimateValue () {
+function estimateValue() {
 	if (value > 0) {
-    return 'wynik dodatni';
+    		return 'wynik dodatni';
 	} else if (value < 0) {
-    return 'wynik ujemny';
+    		return 'wynik ujemny';
 	} else {
-    return ('Aha sprawdzasz liczbe ' + value);
+    		return 'Aha sprawdzasz liczbe ' + value;
 	}
 }
 console.log('Twoj wynik ze zmnienna a: ' + a + ' i zmienna b: ' + b + ' rowna sie: ' + estimateValue());
